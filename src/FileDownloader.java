63ba9474af79da91e703a4a26722c5707419b7df
Jon Andersen <jon.andersen.se@gmail.com>
2010-01-16 21:52:06 +0000

    En klass som laddar ner en fil, krävs ett URL och ett namn på filen för att fungerar. INGA TEST FINNS! det måste åtgärdas men har en hård flickvän som tvingar mig till att spela wow nu istället för att lösa det :(

---
+++ src/FileDownloader.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.net.*;
+
+public class FileDownloader {
+	private BufferedInputStream bis;
+	private FileOutputStream fos;
+	private BufferedOutputStream bos;
+
+	public FileDownloader(String url, String filename)
+			throws MalformedURLException, IOException {
+		bis = new BufferedInputStream(new URL(url).openStream());
+		fos = new FileOutputStream(filename);
+		bos = new BufferedOutputStream(fos, 1024);
+
+		byte[] buf = new byte[4 * 1024]; // 4K buffer
+		int bytesRead;
+		while ((bytesRead = bis.read(buf)) != -1) {
+			bos.write(buf, 0, bytesRead);
+		}
+		bos.close();
+		bis.close();
+	}
+}
