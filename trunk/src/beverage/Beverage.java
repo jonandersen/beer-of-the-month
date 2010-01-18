32bd5ea6e1f26f4c06316b33bcb17daf8d7a8b29
johan.anderholm <johan.anderholm@e4affe78-b978-11de-88a7-9da883da0866>
2010-01-18 10:11:03 +0000

    Enumerator done

--- trunk/src/beverage/Beverage.java
+++ trunk/src/beverage/Beverage.java
@@ -1,16 +1,23 @@
 package beverage;
 
 public class Beverage {
+	public enum Info {
+		ID, NAME, NAME2, PRICE, VOLUME, SALESTART, ENDLEV, TYPE,
+		ORIGIN, ORIGINCOUNTRY, PRODUCER, VINTAGE, TESTVINTAGE, ALCOHOL
+	}
+
 	public String id, name, name2, price, volume, salestart, endlev, type,
-	origin, origincountry, producer, vintage, testvintage, alcohol;
+			origin, origincountry, producer, vintage, testvintage, alcohol;
 	public String[] beverage;
-	public Beverage(String id, String name, String name2, String price, String volume,
-			String salestart, String endlev, String type, String origin, String origincountry,
-			String producer, String vintage, String testvintage, String alcohol){
+
+	public Beverage(String id, String name, String name2, String price,
+			String volume, String salestart, String endlev, String type,
+			String origin, String origincountry, String producer,
+			String vintage, String testvintage, String alcohol) {
 		this.id = id;
 		this.name = name;
 		this.name2 = name2;
 		this.price = price;
-		
+
 	}
 }
