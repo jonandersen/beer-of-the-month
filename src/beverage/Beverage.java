c423f2adadcf88e22ac5c9f0a298ecb266bff022
carlber.marcus <carlber.marcus@gmail.com>
2010-01-18 09:59:48 +0000

    Fixa lite med beverage

---
+++ src/beverage/Beverage.java
@@ -0,0 +1,16 @@
+package beverage;
+
+public class Beverage {
+	public String id, name, name2, price, volume, salestart, endlev, type,
+	origin, origincountry, producer, vintage, testvintage, alcohol;
+	public String[] beverage;
+	public Beverage(String id, String name, String name2, String price, String volume,
+			String salestart, String endlev, String type, String origin, String origincountry,
+			String producer, String vintage, String testvintage, String alcohol){
+		this.id = id;
+		this.name = name;
+		this.name2 = name2;
+		this.price = price;
+		
+	}
+}
