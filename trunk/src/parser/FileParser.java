44ec5613467944a0cb86c0cac86af036e003248b
johan.anderholm <johan.anderholm@e4affe78-b978-11de-88a7-9da883da0866>
2010-01-18 11:15:17 +0000

    ParserFuck

--- trunk/src/parser/FileParser.java
+++ trunk/src/parser/FileParser.java
@@ -1,5 +1,44 @@
 package parser;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 public class FileParser {
+	private BufferedReader bread;
+	
+	public FileParser(File file) {
+		FileReader fread = null;
+		try {
+			fread = new FileReader(file);
+			bread = new BufferedReader(fread);
+		} catch(Exception e) {
+			System.exit(5);
+		}
+	}
+
+	public HashMap<Enum, String> parse(){
+		HashMap<Enum, String> info = new HashMap<Enum, String>();
+		StringTokenizer tokenizer; 
+		try {
+			while(bread.ready()){
+				tokenizer = new StringTokenizer(bread.readLine(), "\t");
+				while(tokenizer.hasMoreTokens()){
+					if(!tokenizer.nextToken().matches(""));
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
