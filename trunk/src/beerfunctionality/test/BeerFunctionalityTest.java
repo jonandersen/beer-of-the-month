7239e1e24e1046b7be9eb7cf817dd6cc35fc2e24
carlber.marcus <carlber.marcus@e4affe78-b978-11de-88a7-9da883da0866>
2010-01-18 15:33:52 +0000

    bläa

--- trunk/src/beerfunctionality/test/BeerFunctionalityTest.java
+++ trunk/src/beerfunctionality/test/BeerFunctionalityTest.java
@@ -29,19 +29,27 @@ public class BeerFunctionalityTest {
 		m1.put(ArticleInfo.ALCOHOL, "10");
 		m1.put(ArticleInfo.VOLUME, "1000");
 		m1.put(ArticleInfo.PRICE, "90");
+		m1.put(ArticleInfo.TYPE, "ÖL");
 		m2.put(ArticleInfo.ID, "2");
 		m2.put(ArticleInfo.ALCOHOL, "100");
 		m2.put(ArticleInfo.VOLUME, "1000");
 		m2.put(ArticleInfo.PRICE, "100");
+		m2.put(ArticleInfo.TYPE, "ÖL");
 		m3.put(ArticleInfo.ID, "3");
 		m3.put(ArticleInfo.ALCOHOL, "1");
 		m3.put(ArticleInfo.VOLUME, "1000");
 		m3.put(ArticleInfo.PRICE, "120");
+		m3.put(ArticleInfo.TYPE, "ÖL");
 		db.add(new Beverage(m1));
 		bev = new Beverage(m2);
 		db.add(bev);
 		db.add(new Beverage(m3));
 		bf = new BeerFunctionality(db);
+	}
+	
+	@Test
+	public void BeerOfTheMonthFunctionallityTest(){
+		assertEquals("Det blev fel", bf.BeerOfTheMonth(), bev);
 	}
 	
 	@Test
