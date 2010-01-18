bab014537a85350d39a597271dee9ad5ad5dbed7
Johan Anderholm <Fulkerson@users.noreply.github.com>
2010-01-18 09:45:47 +0000

    "First" try at parser

---
+++ src/test/FileParserTest.java
@@ -0,0 +1,33 @@
+package test;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author dt08ja5
+ *
+ */
+public class FileParserTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void ParseTest(){
+		
+	}
+
+}
