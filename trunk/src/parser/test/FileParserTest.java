0f1fda2a4eb51c7ec198554a00501995dc9ac5a8
johan.anderholm <johan.anderholm@e4affe78-b978-11de-88a7-9da883da0866>
2010-01-18 14:19:42 +0000

    Major minor change, ArticleInfo. Not Enum.

--- trunk/src/parser/test/FileParserTest.java
+++ trunk/src/parser/test/FileParserTest.java
@@ -1,11 +1,19 @@
 package parser.test;
 
 
+import static org.junit.Assert.*;
+
 import java.io.File;
+import java.util.Collection;
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import beverage.ArticleInfo;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 import parser.FileParser;
 
@@ -15,13 +23,15 @@ import parser.FileParser;
  */
 public class FileParserTest {
 
+	private File file;
+	private FileParser parser;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		File file = new File("/home/johan/testfile");
-		FileParser parser = new FileParser(file);
+		file = new File("/home/johan/testfile");
+		parser = new FileParser(file);
 	}
 
 	/**
@@ -31,9 +41,13 @@ public class FileParserTest {
 	public void tearDown() throws Exception {
 	}
 	
-	@Test
-	public void ParseTest(){
-		
-	}
+//	@Test
+//	public void ParseLineTest(){
+//		HashMap<ArticleInfo, String> map = parser.parseLine("101	one	two	three		four");
+//		Collection<String> list = map.values();
+//		String[] cList = {"101", "one", "two", "three", "", "four"};
+//		for(int i = 0; i < cList.length; ++i)
+//			assertEquals(map.get(i), cList[i]);
+//	}
 
 }
