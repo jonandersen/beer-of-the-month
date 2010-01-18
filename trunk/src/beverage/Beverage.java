2a323c40a079f66ba306774528177d19d5ea8081
johan.anderholm <johan.anderholm@e4affe78-b978-11de-88a7-9da883da0866>
2010-01-18 10:20:46 +0000

    Beverage

--- trunk/src/beverage/Beverage.java
+++ trunk/src/beverage/Beverage.java
@@ -1,23 +1,11 @@
 package beverage;
 
+import java.util.Map;
+
 public class Beverage {
-	public enum Info {
-		ID, NAME, NAME2, PRICE, VOLUME, SALESTART, ENDLEV, TYPE,
-		ORIGIN, ORIGINCOUNTRY, PRODUCER, VINTAGE, TESTVINTAGE, ALCOHOL
-	}
+	private Map<Enum, String> info;
 
-	public String id, name, name2, price, volume, salestart, endlev, type,
-			origin, origincountry, producer, vintage, testvintage, alcohol;
-	public String[] beverage;
-
-	public Beverage(String id, String name, String name2, String price,
-			String volume, String salestart, String endlev, String type,
-			String origin, String origincountry, String producer,
-			String vintage, String testvintage, String alcohol) {
-		this.id = id;
-		this.name = name;
-		this.name2 = name2;
-		this.price = price;
-
+	public Beverage(Map<Enum, String> info) {
+		this.info = info;
 	}
 }
