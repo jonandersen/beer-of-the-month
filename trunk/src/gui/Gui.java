e729eb752e602f0e574406a77bb2941049a69b89
jon.andersen88 <jon.andersen88@e4affe78-b978-11de-88a7-9da883da0866>
2010-04-16 20:11:24 +0000

    Fixed a nasty bug, and started enhancing the appearans

--- trunk/src/gui/Gui.java
+++ trunk/src/gui/Gui.java
@@ -1,4 +1,6 @@
 package gui;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
@@ -9,6 +11,7 @@ import server.Server;
 import model.BeerFunctionality;
 import model.Beverage;
 import model.Database;
+import net.miginfocom.swing.MigLayout;
 /*
  * Created by JFormDesigner on Sat Feb 06 15:54:42 CET 2010
  */
@@ -60,95 +63,12 @@ public class Gui extends JFrame {
 	
 
 
-	private void okButtonActionPerformed(ActionEvent e)  {			
-		textPane1.setText("");			
-		if(set.rollBeer()){			
-			if(set.checkStock()){
-				Beverage beer = null;
-				try {
-					if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
-						beer = bf.BeerOfTheMonthInStock();
-					}else{
-						beer = bf.BeerOfTheMonthInStock(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
-					}
-				} catch (IOException e1) {					
-					e1.printStackTrace();
-					System.exit(0);
-				}
-				if(beer==null){
-					textPane1.setText(textPane1.getText() + "Beer of the Month: "+ "Couldn't find one =(" + "\n");
-				}else{
-					textPane1.setText(textPane1.getText() + "Beer of the Month: "+ beer.toString() + " In stock:" + beer.getStockCount() + "\n");
-				}
-			}else{
-				Beverage beer;
-				if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
-					 beer = bf.BeerOfTheMonth();
-				}else{
-					 beer = bf.BeerOfTheMonth(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
-				}
-				
-				if(beer==null){
-					textPane1.setText(textPane1.getText() + "Beer of the Month: "+ "Couldn't find one =(" + "\n");
-				}else{
-					textPane1.setText(textPane1.getText() + "Beer of the Month: "+ beer.toString() + "\n");
-				}
-				
-			}
-		}
-		if(set.rollWine()){
-			if(set.checkStock()){
-				Beverage wine = null;
-				try {
-					if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
-						wine = bf.WineOfTheMonthInStock();
-					}else{
-						wine = bf.WineOfTheMonthInStock(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
-					}
-					 
-				} catch (IOException e1) {					
-					e1.printStackTrace();
-					System.exit(0);
-				}
-				if(wine==null){
-					textPane1.setText(textPane1.getText() + "Wine of the Month: " + "Couldn't find one =(" + "\n");
-				}else{
-					textPane1.setText(textPane1.getText() + "Wine of the Month: " + wine.toString() + " In stock:" + wine.getStockCount() + "\n");
-				}
-			}else{
-				Beverage wine = null;
-				if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
-					wine = bf.WineOfTheMonth();
-				}else{
-					wine = bf.WineOfTheMonth(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
-				}
-				if(wine==null){
-					textPane1.setText(textPane1.getText() + "Wine of the Month: " + "Couldn't find one =(" + "\n");
-				}else{
-					textPane1.setText(textPane1.getText() + "Wine of the Month: " + wine.toString() + "\n");
-				}
-					
-				}
-		}
-		if(set.rollBeverage()){
-			if(set.checkStock()){
-				Beverage beverage = null;
-				try {
-					beverage = bf.BeverageOfTheMonthInStock();
-				} catch (IOException e1) {					
-					e1.printStackTrace();
-					System.exit(0);
-				}
-				textPane1.setText(textPane1.getText() + "Beverage of the Month: " + beverage.toString() + "\n");
-			}else{
-				Beverage beverage = bf.BeverageOfTheMonth();
-				textPane1.setText(textPane1.getText() + "Beverage of the Month: " + beverage.toString() + "\n");
-			}
-		}
-		if(set.bFB()){
-			Beverage beverage = bf.bangForTheBuck();
-			textPane1.setText(textPane1.getText() + "Most Bang for the Buck: " + beverage.toString() + "\n");
-		}		
+	private void okButtonActionPerformed(ActionEvent e)  {
+		if(set.all()){
+			InfoArea.setText("Evaluating important beer decisions");
+			okButton.setEnabled(false);
+			loadData();
+		}				
 	}	
 
 	private void configureActionPerformed(ActionEvent e) {
@@ -165,6 +85,110 @@ public class Gui extends JFrame {
 			}
 		System.exit(1);
 	}
+	 
+	 private void loadData(){
+		 Thread thread = new Thread(new Runnable() {
+		        public void run() {    
+		        	textPane1.setText("");			
+					if(set.rollBeer()){			
+						if(set.checkStock()){
+							Beverage beer = null;
+							try {
+								if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
+									beer = bf.BeerOfTheMonthInStock();
+								}else{
+									beer = bf.BeerOfTheMonthInStock(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
+								}
+							} catch (IOException e1) {					
+								e1.printStackTrace();
+								System.exit(0);
+							}
+							if(beer==null){
+								textPane1.setText(textPane1.getText() + "Beer of the Month: "+ "Couldn't find one =(" + "\n");
+							}else{
+								textPane1.setText(textPane1.getText() + "Beer of the Month: "+ beer.toString() + " In stock:" + beer.getStockCount() + "\n");
+							}
+						}else{
+							Beverage beer;
+							if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
+								 beer = bf.BeerOfTheMonth();
+							}else{
+								 beer = bf.BeerOfTheMonth(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
+							}
+							
+							if(beer==null){
+								textPane1.setText(textPane1.getText() + "Beer of the Month: "+ "Couldn't find one =(" + "\n");
+							}else{
+								textPane1.setText(textPane1.getText() + "Beer of the Month: "+ beer.toString() + "\n");
+							}
+							
+						}				
+					}
+					if(set.rollWine()){
+						if(set.checkStock()){
+							Beverage wine = null;
+							try {
+								if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
+									wine = bf.WineOfTheMonthInStock();
+								}else{
+									wine = bf.WineOfTheMonthInStock(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
+								}
+								 
+							} catch (IOException e1) {					
+								e1.printStackTrace();
+								System.exit(0);
+							}
+							if(wine==null){
+								textPane1.setText(textPane1.getText() + "Wine of the Month: " + "Couldn't find one =(" + "\n");
+							}else{
+								textPane1.setText(textPane1.getText() + "Wine of the Month: " + wine.toString() + " In stock:" + wine.getStockCount() + "\n");
+							}
+						}else{
+							Beverage wine = null;
+							if(set.getPriceLessOrEqualsThen() <= 0 && set.getVolume()[0] <=0 && set.getVolume()[1] <=0 && set.getAlco()[0]<=0 && set.getAlco()[1]<=0){
+								wine = bf.WineOfTheMonth();
+							}else{
+								wine = bf.WineOfTheMonth(set.getPriceLessOrEqualsThen(),set.getVolume(), set.getAlco());
+							}
+							if(wine==null){
+								textPane1.setText(textPane1.getText() + "Wine of the Month: " + "Couldn't find one =(" + "\n");
+							}else{
+								textPane1.setText(textPane1.getText() + "Wine of the Month: " + wine.toString() + "\n");
+							}
+								
+							}
+					}
+					if(set.rollBeverage()){
+						if(set.checkStock()){
+							Beverage beverage = null;
+							try {
+								beverage = bf.BeverageOfTheMonthInStock();
+							} catch (IOException e1) {					
+								e1.printStackTrace();
+								System.exit(0);
+							}
+							textPane1.setText(textPane1.getText() + "Beverage of the Month: " + beverage.toString() + "\n");
+						}else{
+							Beverage beverage = bf.BeverageOfTheMonth();
+							textPane1.setText(textPane1.getText() + "Beverage of the Month: " + beverage.toString() + "\n");
+						}
+					}
+					if(set.bFB()){
+						Beverage beverage = bf.bangForTheBuck();
+						textPane1.setText(textPane1.getText() + "Most Bang for the Buck: " + beverage.toString() + "\n");
+					}
+					
+					InfoArea.setText("Done evaluating, check above for beverage");
+					okButton.setEnabled(true);
+										
+		        }});
+		    	// Keep gui responsive to user input.
+		    thread.setPriority(Thread.NORM_PRIORITY);  // 5, EDT = 6
+		    thread.start();			    
+	 }
+	 
+	 
+
 
 	private void initComponents() {
 		// JFormDesigner - Component initialization - MODIFY AT WILL!  //GEN-BEGIN:initComponents
@@ -176,12 +200,11 @@ public class Gui extends JFrame {
 		menuItem2 = new JMenuItem();
 		menuItem2b = new JMenuItem();
 		dialogPane = new JPanel();
+		BorderLayout dialogPaneLayout = new BorderLayout();
+		dialogPane.setLayout(dialogPaneLayout);
 		contentPanel = new JPanel();
 		scrollPane1 = new JScrollPane();
 		textPane1 = new JTextPane();
-		buttonBar = new JPanel();
-		okButton = new JButton();
-		cancelButton = new JButton();
 
 		//======== this ========
 		Container contentPane = getContentPane();
@@ -273,7 +296,7 @@ public class Gui extends JFrame {
 		//======== dialogPane ========
 		{
 			dialogPane.setBorder(new EmptyBorder(12, 12, 12, 12));
-
+			
 
 
 			//======== contentPanel ========
@@ -285,6 +308,7 @@ public class Gui extends JFrame {
 					//---- textPane1 ----
 					textPane1.setEditable(false);
 					scrollPane1.setViewportView(textPane1);
+					textPane1.setPreferredSize(new java.awt.Dimension(417, 157));
 				}
 
 				GroupLayout contentPanelLayout = new GroupLayout(contentPanel);
@@ -305,40 +329,73 @@ public class Gui extends JFrame {
 				);
 			}
 			dialogPane.add(contentPanel, BorderLayout.CENTER);
-
+			{
+				buttonBar = new JPanel();
+				dialogPane.add(buttonBar, BorderLayout.NORTH);
+				FormLayout buttonBarLayout = new FormLayout(
+						"max(p;5dlu), max(p;5dlu), max(p;5dlu), max(p;5dlu)", 
+						"max(p;5dlu), max(p;5dlu), max(p;5dlu), max(p;5dlu)");
+				buttonBar.setLayout(buttonBarLayout);				
+				buttonBar.setPreferredSize(new java.awt.Dimension(640, 60));
+				{	
+					ImageIcon icon = new ImageIcon("Images/roll.png");
+					okButton = new JButton("Roll", icon);
+					buttonBar.add(okButton, new CellConstraints("2, 1, 1, 1, default, default"));
+					okButton.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {					
+							okButtonActionPerformed(e);						
+						}
+					});
+				}
+				{	
+					ImageIcon icon = new ImageIcon("Images/exit.png");
+					
+					cancelButton = new JButton("Exit", icon);
+					buttonBar.add(cancelButton, new CellConstraints("3, 1, 1, 1, default, default"));					
+					cancelButton.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							cancelButtonActionPerformed(e);
+						}
+					});
+				}
+				{	
+					ImageIcon icon = new ImageIcon("Images/settings.png");
+					settings = new JButton("Settings", icon);
+					buttonBar.add(settings, new CellConstraints("4, 1, 1, 1, default, default"));					
+				}
+			}
+			
 			//======== buttonBar ========
 			{
-				buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
-				buttonBar.setLayout(new GridBagLayout());
-				((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 85, 80};
-				((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0, 0.0};
 
 				//---- okButton ----
-				okButton.setText("Roll");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						okButtonActionPerformed(e);
-					}
-				});
-				buttonBar.add(okButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
-					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-					new Insets(0, 0, 0, 5), 0, 0));
 
 				//---- cancelButton ----
-				cancelButton.setText("Exit");
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						cancelButtonActionPerformed(e);
-					}
-				});
-				buttonBar.add(cancelButton, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
-					GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-					new Insets(0, 0, 0, 0), 0, 0));
 			}
-			dialogPane.add(buttonBar, BorderLayout.SOUTH);
+			{
+				progress = new JPanel();				
+				GroupLayout progressLayout = new GroupLayout((JComponent)progress);
+				progress.setLayout(progressLayout);
+				dialogPane.add(progress, BorderLayout.SOUTH);
+				progress.setPreferredSize(new java.awt.Dimension(640, 51));
+				{
+					InfoArea = new JTextField();
+					InfoArea.setFont(new java.awt.Font("Tahoma",0,28));
+					InfoArea.setEditable(false);
+					InfoArea.setSize(620, 51);
+				}
+
+					progressLayout.setHorizontalGroup(progressLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(InfoArea, 0, 620, Short.MAX_VALUE)
+					.addContainerGap());
+					progressLayout.setVerticalGroup(progressLayout.createSequentialGroup()
+					.addComponent(InfoArea, 0, 45, Short.MAX_VALUE)
+					.addGap(6));
+			}
 		}
 		contentPane.add(dialogPane, BorderLayout.NORTH);
-		dialogPane.setPreferredSize(new java.awt.Dimension(664, 289));
+		dialogPane.setPreferredSize(new java.awt.Dimension(664, 356));
 		pack();
 		setLocationRelativeTo(getOwner());
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
@@ -355,6 +412,9 @@ public class Gui extends JFrame {
 	private JMenu menu1;
 	private JMenu menu2;
 	private JMenu jHistory;
+	private JButton settings;
+	private JTextField InfoArea;
+	private JPanel progress;
 	private JMenuItem jItemHistory;
 	private JMenuItem menuItem1;
 	private JMenuItem menuItem2;
