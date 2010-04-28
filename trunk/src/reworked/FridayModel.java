db1366cd61c56d8f332b9d2eef9552ea455238ea
jon.andersen88 <jon.andersen88@e4affe78-b978-11de-88a7-9da883da0866>
2010-04-28 13:01:28 +0000

    metod i firdaymodel

--- trunk/src/reworked/FridayModel.java
+++ trunk/src/reworked/FridayModel.java
@@ -1,7 +1,18 @@
 package reworked;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Calendar;
 import java.util.Observable;
+import java.util.Scanner;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import exception.BotMException;
+
+import util.FileDownloader;
 
 
 public class FridayModel extends Observable{
@@ -20,10 +31,24 @@ public class FridayModel extends Observable{
 				answer = "Ja";
 			}else{
 				answer = "Nej";
-			}
+			}			
 			setChanged();
 			notifyObservers();
 		}
+		
+//		private void check(){		
+//			ScriptEngineManager manager = new ScriptEngineManager();
+//		    ScriptEngine engine = manager.getEngineByName("javascript");
+//		    try {
+//		      engine.put("name", "abcde");
+//		      engine.eval("var output = '';for (i = 0; i <= name.length; i++) {"
+//		          + "  output = name.charAt(i)+'-' + output" + "}");
+//		      String name = (String) engine.get("output");
+//		      System.out.println(name);
+//		    } catch (ScriptException e) {
+//		      System.err.println(e);
+//		    }
+//		}
 		
 		public String getAnswer(){
 			return answer;
